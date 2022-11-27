27365006a2ce2199ee0fd8ac84f7ef06a853d335
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2022-11-27 16:52:32 -0300

    Add navbar styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './App.css'
 import './assets/styles/custom.scss'
+import Navbar from "./components/Navbar";
 
-function App() {
-  return (
-      <h1>Hello World</h1>
-  );
+
+const App = () => {
+    return (
+        <>
+            <Navbar></Navbar>
+        </>
+    );
 }
 
 export default App;
